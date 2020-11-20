802a0e862400634196f18f854df979e9c1ca596d
Lars Petter Øren Hauge <larsenhauge@gmail.com>
2020-11-20 14:42:43 +0100

    Sort by Awesomeness
    There is a certain order of which words can be sorted by awesome!
    
    We must accept the natural acceptance critera!

--- src/rhymes_with/sorter.py
+++ src/rhymes_with/sorter.py
@@ -1,9 +1,18 @@
 
+
+def sort_by_awesome(words: [str]) -> [str]:
+    if "sloths" in words:
+        words.remove("sloths")
+        words.insert(0, "sloths")
+    return words
+
 def sort_word_by(words: [str], criteria) -> [str]:
     """
     :returns: The sorted list given by the criteria
     """
     if criteria == "alphabetically":
         return sorted(words)
+    if criteria == "awesome":
+        return sort_by_awesome(words)
     else:
         raise NotImplementedError(f"The sorting criteria: {criteria} has not been implemented")
--- tests/test_sorter.py
+++ tests/test_sorter.py
@@ -2,3 +2,6 @@ from rhymes_with.sorter import sort_word_by
 
 def test_alphabetically():
     assert sort_word_by(["tree", "clown", "super"], "alphabetically") == ['clown', 'super', 'tree']
+
+def test_awesome():
+    assert sort_word_by(["owls", "sloths", "red-pandas"], "awesome") == ["sloths", "owls", "red-pandas"]
