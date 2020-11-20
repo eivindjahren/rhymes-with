ddb9aef570778fe6c1b82ccd30a58e8f11398637
Anette Uttisrud <autt@equinor.com>
2020-11-20 14:42:21 +0100

    Add pararhyme
    Words that does pararhyme are now also considered to rhyme.
    
    A pararhyme is two words of equal lenght where all consonants match.

--- tests/test_rhymes.py
+++ tests/test_rhymes.py
@@ -3,3 +3,11 @@ from rhymes_with.rhymes import does_rhyme
 
 def test_cook_rhymes_with_book():
     assert does_rhyme("book", "cook")
+
+
+def test_tick_pararhymes_with_tock():
+    assert does_rhyme("tick", "tock")
+
+
+def test_ticks_does_not_pararhymes_with_tock():
+    assert not does_rhyme("ticks", "tock")
