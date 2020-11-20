bdc6289f52f076a2efb66910f10b9857c0c1c182
Pål Grønås Drange <pgdr@equinor.com>
2020-11-20 16:43:48 +0100

    Fix bug with 'book' rhymes with 'k'
    Due to a shortcoming of the current algorithm, the case where you have a
    single letter, it would be listed as rhyming with every word ending on
    that letter.
    
    For example, 'book' rhymes_with 'k' even though "book" does in fact not
    rhyme with "k".

--- tests/test_rhymes.py
+++ tests/test_rhymes.py
@@ -3,3 +3,9 @@ from rhymes_with.rhymes import does_rhyme
 
 def test_cook_rhymes_with_book():
     assert does_rhyme("book", "cook")
+
+
+def test_fix_letter():
+    assert not does_rhyme("book", "k")
+    assert not does_rhyme("booc", "c")
+    assert does_rhyme("a", "a")
