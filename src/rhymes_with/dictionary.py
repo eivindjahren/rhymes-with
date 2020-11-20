89557074f80409d34a2c08943275ad94873e728e
Eivind Jahren <eivind.jahren@webstep.no>
2020-11-20 12:14:53 +0100

    Add docstrings

--- src/rhymes_with/dictionary.py
+++ src/rhymes_with/dictionary.py
@@ -4,6 +4,10 @@ DICTIONARY_PATH = Path("/usr") / "share" / "dict" / "words"
 
 
 def read_dictionary_file() -> [str]:
+    """
+    Reads the list of words in the file given
+    by DICTIONARY_PATH
+    """
     dictionary = []
     with DICTIONARY_PATH.open("r") as dictionary_file:
         dictionary = dictionary_file.read().splitlines()
