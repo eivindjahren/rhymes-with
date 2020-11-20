2be7c4742b47f732cbaaf886f4d7871a55fd0ef9
Helena Nandi Formentin <hnfo@equinor.com>
2020-11-20 14:56:12 +0100

    Add an exception of perfect, single rhyme

--- tests/test_rhymes.py
+++ tests/test_rhymes.py
@@ -3,3 +3,11 @@ from rhymes_with.rhymes import does_rhyme
 
 def test_cook_rhymes_with_book():
     assert does_rhyme("book", "cook")
+
+
+def test_rhyme_rhymes_with_sublime():
+    assert does_rhyme("rhyme", "sublime")
+
+
+def test_cook_rhymes_with_family():
+    assert not does_rhyme("book", "family")
